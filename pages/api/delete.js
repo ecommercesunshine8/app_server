96afbd8ad00672cf2efa4c82693d7bd54e9b2b36
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-05-02 01:54:36 +0600

    u

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -1,20 +1,22 @@
 import { collection, getDocs, deleteDoc } from 'firebase/firestore';
 import { database } from '../../database/firebase.js';
 
+
+
 export default async function handler(req, res) {
-  try {
-    const notificationsCollection = collection(database, 'notifications');
-    const notificationsSnapshot = await getDocs(notificationsCollection);
-    const batch = database.batch();
+    try {
+        const notificationsCollection = collection(database, 'notifications');
+        const notificationsSnapshot = await getDocs(notificationsCollection);
+        const batch = database.batch();
 
-    notificationsSnapshot.forEach((doc) => {
-      batch.delete(doc.ref);
-    });
+        notificationsSnapshot.forEach((doc) => {
+            batch.delete(doc.ref);
+        });
 
-    await batch.commit();
-    res.status(200).json({ message: 'Notifications deleted successfully' });
-  } catch (error) {
-    console.error('Error deleting notifications: ', error);
-    res.status(500).json({ message: 'Server error' });
-  }
+        await batch.commit();
+        res.status(200).json({ message: 'Notifications deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting notifications: ', error);
+        res.status(500).json({ message: 'Server error', errorMessage: error.message });
+    }
 }
